71d386ae49937f733914413c56073b3e45eff434
Hawley, Tony (LDN-WSW) <THawley@webershandwick.com>
2015-07-31 11:45:05 +0100

    First commit

---
+++ acf-post-id-v4.php
@@ -0,0 +1,212 @@
+<?php
+
+class acf_field_post_id extends acf_field {
+
+	// vars
+	var $settings, // will hold info such as dir / path
+		$defaults; // will hold default field options
+
+
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function __construct()
+	{
+		// vars
+		$this->name = 'Post ID';
+		$this->label = __('Post ID');
+		$this->category = __("Relational",'acf'); // Basic, Content, Choice, etc
+		$this->defaults = array(
+			// add default here to merge into your field.
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'preview_size' => 'thumbnail'
+		);
+
+
+		// do not delete!
+    	parent::__construct();
+
+
+    	// settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
+
+	}
+
+
+	/*
+	*  load_field()
+	*
+	*  This filter is appied to the $field after it is loaded from the database
+	*
+	*  @type filter
+	*  @since 3.6
+	*  @date 23/01/13
+	*
+	*  @param $field - the field array holding all the field options
+	*
+	*  @return $field - the field array holding all the field options
+	*/
+
+	function load_field( $field )
+	{
+
+		// validate post_type
+		if( !$field['post_type'] || !is_array($field['post_type']) || in_array('', $field['post_type']) )
+		{
+			$field['post_type'] = array( 'all' );
+		}
+
+
+		// return
+		return $field;
+	}
+
+
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function create_field( $field )
+	{
+		// let post_object create the field
+		$field['type'] = 'post_object';
+
+		do_action('acf/create_field', $field );
+	}
+
+
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+
+	function create_options( $field )
+	{
+		$key = $field['name'];
+
+		?>
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label for=""><?php _e("Post Type",'acf'); ?></label>
+	</td>
+	<td>
+		<?php
+
+		$choices = array(
+			'all'	=>	__("All",'acf')
+		);
+		$choices = apply_filters('acf/get_post_types', $choices);
+
+
+		do_action('acf/create_field', array(
+			'type'	=>	'select',
+			'name'	=>	'fields['.$key.'][post_type]',
+			'value'	=>	$field['post_type'],
+			'choices'	=>	$choices,
+			'multiple'	=>	1,
+		));
+
+		?>
+	</td>
+</tr>
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Allow Null?",'acf'); ?></label>
+	</td>
+	<td>
+		<?php
+
+		do_action('acf/create_field', array(
+			'type'	=>	'radio',
+			'name'	=>	'fields['.$key.'][allow_null]',
+			'value'	=>	$field['allow_null'],
+			'choices'	=>	array(
+				1	=>	__("Yes",'acf'),
+				0	=>	__("No",'acf'),
+			),
+			'layout'	=>	'horizontal',
+		));
+
+		?>
+	</td>
+</tr>
+
+		<?php
+
+	}
+
+
+	/*
+	*  format_value_for_api()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+
+	function format_value_for_api( $value, $post_id, $field )
+	{
+		if( !$value )
+		{
+			return false;
+		}
+
+		if( $value == 'null' )
+		{
+			return false;
+		}
+
+		if( is_array($value) )
+		{
+			foreach( $value as $k => $v )
+			{
+				$value[ $k ] = get_the_id($v);
+			}
+		}
+		else
+		{
+			$value = get_the_id($value);
+		}
+
+		return $value;
+	}
+
+}
+
+new acf_field_post_id();
+
+?>
